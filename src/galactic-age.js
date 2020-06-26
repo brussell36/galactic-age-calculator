c7ab8d6bb3e93c864868da8bed75ea40a760e507
Ben Russell <benjaminrussell36@gmail.com>
2020-06-26 09:58:40 -0700

    Add logic to pass Jupiter age test. Pass test.

--- src/galactic-age.js
+++ src/galactic-age.js
@@ -4,26 +4,27 @@ export class UserAge {
   }
 
   findDays() {
-    let userDays = this.age * 365;
+    const userDays = this.age * 365;
     return userDays;
   }
 
   merAge(userDays) {
-    let merYears = userDays / 88;
+    const merYears = userDays / 88;
     return merYears;
   }
 
   venusAge(userDays) {
-    let venusYears = userDays / 225;
+    const venusYears = userDays / 225;
     return venusYears;
   }
 
   marsAge(userDays) {
-    let marsYears = userDays / 687;
+    const marsYears = userDays / 687;
     return marsYears;
   }
 
   jupAge(userDays) {
-    
+    const jupYears = userDays / (11.8 * 365);
+    return jupYears;
   }
 }
